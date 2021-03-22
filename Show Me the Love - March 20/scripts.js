08e2a97c5b749c823ebababee7db2443ce16cbae
Izebel <75639652+I-z-e-b-e-l@users.noreply.github.com>
2021-03-22 17:27:33 -0400

    Add files via upload

---
+++ Show Me the Love - March 20/scripts.js
@@ -0,0 +1,48 @@
+function evaluateLove() {
+    let urName = prompt (`What is your name?`);
+    let urColor = prompt (`What is your favoirite color, ${urName}?`);
+
+    let message = ""
+
+if (urColor == "purple"|"teal"|"aqua") {
+     message += `💕How you doin...😍`;
+    alert (message);
+} else {
+    message += `💔 Bye Phelipe....`;
+    alert (message);  
+} 
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//     switch(urColor) {
+//         case "purple";
+//         case "teal";
+//         case "aqua";
+//         alert (`💕How you doin...😍`)
+//         break;
+//     default: alert (`💔 Bye Phelipe....`)
+//     }
+
+
+// }
+
+// let message = "";
+
+
+
+
+
+// let message = 
